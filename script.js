abc2e1eb77d311e00b9a76ce7f1089907ab6bd5f
bodargq <114524261+bodargq@users.noreply.github.com>
2022-09-29 02:13:05 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+VANTA.TRUNK({
+    el: "#your-element-selector",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    chaos: 2.50,
+    spacing: -1.50
+  })
