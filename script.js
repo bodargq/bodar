869d37f72983f31dd2557e602b38c3fa1edcc3fe
bodargq <114524261+bodargq@users.noreply.github.com>
2022-09-29 13:02:30 +0500

    add "gotoluna", add <main>

--- script.js
+++ script.js
@@ -8,5 +8,39 @@ VANTA.TRUNK({
     scale: 1.00,
     scaleMobile: 1.00,
     chaos: 2.50,
-    spacing: -1.50
-  })
+    spacing: -1.70
+  });
+
+  VANTA.DOTS({
+    el: "#your-element-selector2",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x98465f,
+    color2: 0x850ff,
+    backgroundColor: 0x222426,
+    spacing: 22.00,
+    showLines: false
+  });
+
+var daq = document.getElementsByTagName('img');
+
+
+//document.getElementsByClassName('glbodarluna').style.display = 'none';
+var imgmas = document.getElementsByTagName('img');
+console.log(imgmas.length);
+imgmas[2].style.display = 'none';
+
+function backbodar(){
+    document.querySelector('button').style.display = 'none';
+    imgmas[3].style.display = 'none';
+    imgmas[2].style.display = 'block';
+    setTimeout(() => {
+      imgmas[3].style.display = 'block';
+      imgmas[2].style.display = 'none';
+      document.querySelector('button').style.display = 'block';
+    }, 4000);}
